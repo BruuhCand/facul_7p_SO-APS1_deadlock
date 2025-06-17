b536b969c444f28c2fde403adcd75dafaa389494
BruuhCand <brunacandidorosa@yahoo.com.br>
2025-06-16 23:14:37 -0300

    fix: adição de ordenação

--- Simulador_deadlock/Desfragmentador.cs
+++ Simulador_deadlock/Desfragmentador.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Simulador_deadlock
 {
     public class Desfragmentador
     {
+        private static readonly object _consoleLock = new object();
+
         public void Iniciar()
         {
-            int numThreads = Environment.ProcessorCount; // 100% de todos os núcleos
+            int numThreads = Environment.ProcessorCount;
             Console.WriteLine($"Usando {numThreads} threads para simular desfragmentação...");
 
             for (int i = 0; i < numThreads; i++)
@@ -19,94 +17,79 @@ namespace Simulador_deadlock
                 var thread = new Thread(() =>
                 {
                     Random random = new Random();
-                    //List<double> blocos = new List<double>();
-                    double?[] blocos = new double?[100_000_000];
-                    while (true)
+                    int?[] blocos = new int?[100_000_000];
+
+                    
+                    for (int j = 0; j < 100_000_000; j++)
                     {
-                        double x = 0;
-                        for (int j = 0; j < 100_000_000; j++)
-                        {
-                            x += Math.Sqrt(j) * Math.Sin(j) * Math.Log(j + 1);  // CPU-bound
-
-                            int numeroAleatorio = random.Next(0, 50);
-                            blocos[j] = numeroAleatorio;
-
-                           
-
-                           
-                            //blocos.Add(x);
-                            //blocos.Sort();
-
-                        }
-
-                        for(int j = 0; j < 50; j++)
-                        {
-                           
-                            int numeroAleatorio = random.Next(0, 100_000);
-
-                            blocos[numeroAleatorio] = null;
-                        }
-
-                        Console.WriteLine("\n\nARRAY COM NULOS\n\n");
-                        for (int j = 0; j < 100; j++)
-                        {
-                            if (blocos[j].HasValue)
-                                Console.Write($"{blocos[j].Value:F2} ");
-                            else
-                                Console.Write("null ");
-                        }
-
-
-                        //for (int j = 0; j < 100_000_000; j++)
-                        //{
-                        //    if (blocos[j] == null)
-                        //    {
-                        //        for(int k = 99_000_000; k > 0; k--)
-                        //        {
-                        //            if(blocos[k] != null)
-                        //            {
-                        //                blocos[j] = blocos[k];
-                        //                blocos[k] = null;
-                        //                break;
-                        //            }
-                        //        }
-                        //    }
-
-                        //}
-
-                        int writeIndex = 0;
-                        int readIndex = 0;
-
-                        while (readIndex < blocos.Length)
-                        {
-                            if (blocos[readIndex].HasValue)
-                            {
-                                blocos[writeIndex] = blocos[readIndex];
-                                if (writeIndex != readIndex)
-                                    blocos[readIndex] = null;
-
-                                writeIndex++;
-                            }
-                            readIndex++;
-                        }
-
-                        Console.WriteLine("\n\nARRAY DESFRAGMENTADO\n\n");
-                        for (int j = 0; j < 100; j++)
-                        {
-                            if (blocos[j].HasValue)
-                                Console.Write($"{blocos[j].Value:F2} ");
-                            else
-                                Console.Write("null ");
-                        }
-
-
-
-
+                        blocos[j] = random.Next(0, 1000);
                     }
+
+                   
+                    for (int j = 0; j < 100; j++)
+                    {
+                        blocos[random.Next(0, 250)] = null;
+                    }
+
+                 
+                    ImprimirEstado("ARRAY COM NULOS", blocos);
+
+                   
+                    int writeIndex = 0;
+                    for (int readIndex = 0; readIndex < blocos.Length; readIndex++)
+                    {
+                        if (blocos[readIndex].HasValue)
+                        {
+                            blocos[writeIndex] = blocos[readIndex];
+                            if (writeIndex != readIndex)
+                                blocos[readIndex] = null;
+                            writeIndex++;
+                        }
+                    }
+
+                    
+                    ImprimirEstado("ARRAY DESFRAGMENTADO", blocos);
+
+                   
+                    int[] contagem = new int[100];
+                    for (int j = 0; j < writeIndex; j++)
+                    {
+                        if (blocos[j].HasValue)
+                            contagem[blocos[j].Value]++;
+                    }
+
+                    int index = 0;
+                    for (int valor = 0; valor < contagem.Length; valor++)
+                    {
+                        for (int c = 0; c < contagem[valor]; c++)
+                        {
+                            blocos[index++] = valor;
+                        }
+                    }
+
+                    for (int j = index; j < blocos.Length; j++)
+                        blocos[j] = null;
+
+                    
+                    ImprimirEstado("ARRAY ORGANIZADO POR VALORES IGUAIS", blocos);
+
                 });
 
                 thread.IsBackground = true;
                 thread.Start();
+            }
+        }
+
+        private void ImprimirEstado(string titulo, int?[] blocos)
+        {
+            lock (_consoleLock)
+            {
+                Console.WriteLine($"\n\n{titulo}\n");
+                for (int j = 0; j < 100; j++)
+                {
+                    Console.Write(blocos[j]?.ToString("D2") + " " ?? "null ");
+                }
+                Console.WriteLine();
             }
         }
     }
